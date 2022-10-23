5e84802f98c922d729514d16e67be16043d23e86
Vishal <90440977+Vishalrai2002@users.noreply.github.com>
2022-10-23 08:07:56 +0530

    Create 23Day Set Mismatch.cpp

---
+++ 23Day Set Mismatch.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int>ans;
+        map<int,int>mp;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+        }
+        for(auto it:mp){
+            if(it.second==2){
+                ans.push_back(it.first);
+            }
+        }
+        int sum=0;
+        int mainsum=0;
+        for(int i=1;i<=n;i++){
+            mainsum+=i;
+        }
+        // cout<<mainsum<<endl;
+        set<int>st;
+        for(int i=0;i<n;i++){
+            st.insert(nums[i]);
+        }
+        for(auto it:st){
+            sum+=it;
+        }
+        // cout<<sum<<endl;
+        // cout<<ans[0]<<endl;
+        ans.push_back(mainsum-sum);
+        
+        return ans;
+    }
+};
